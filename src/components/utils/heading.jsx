295d4901c221b8e8f168083307be66a7c22e4713
Jompa14 <joao.buchholz@gmail.com>
2023-03-29 16:42:56 -0300

    Heading component ok

---
+++ src/components/utils/heading.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const Heading = ({ size = 'M', color = 'black', tag = 'h1', title }) => {
+  const getSizeProps = (size) => {
+    switch (size) {
+      case 'XL':
+        return {
+          fontSize: '250px',
+          lineHeight: '200px',
+          letterSpacing: '-5px',
+        };
+      case 'L':
+        return {
+          fontSize: '96px',
+          lineHeight: '80px',
+          letterSpacing: '-2px',
+        };
+      case 'S':
+        return {
+          fontSize: '32px',
+          lineHeight: '40px',
+          letterSpacing: '0',
+        };
+      default:
+        return {
+          fontSize: '72px',
+          lineHeight: '64px',
+          letterSpacing: '-2px',
+        };
+    }
+  };
+
+  const sizeProps = getSizeProps(size);
+
+  const Tag = tag;
+
+  return (
+    <Tag
+      className={`font-bold text-${color} ${sizeProps.fontSize} leading-${sizeProps.lineHeight} tracking-${sizeProps.letterSpacing}`}
+    >
+      {title}
+    </Tag>
+  );
+};
+
+export default Heading;
