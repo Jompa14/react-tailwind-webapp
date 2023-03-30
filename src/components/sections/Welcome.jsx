1e612bfea3db9cda3867e3154b0913ddf76f65c3
Jompa14 <joao.buchholz@gmail.com>
2023-03-30 17:54:35 -0300

    min font size heading

--- src/components/sections/Welcome.jsx
+++ src/components/sections/Welcome.jsx
@@ -1,17 +1,54 @@
 import React from 'react';
 import Heading from '../utils/heading.jsx'
 
+import welcomeImage from '../../images/home/desktop/image-welcome.jpg';
+
 const Welcome = () => {
     
     return(
-        <div>
-            <Heading 
-                size="XL" 
-                color="text-grey-light" 
-                tag='h2' 
-                title="Welcome" 
-            />
-            <p>oi</p>
+        <div className="mt-20">
+            <div className="absolute">
+                <Heading 
+                    size="XL" 
+                    color="text-[#e2e2e2]" 
+                    tag="h2" 
+                    title="Welcome" 
+                />
+            </div>
+            <div 
+                id="textPlusImage"
+                className="flex lg:justify-end  items-end"
+            >
+                <div 
+                    id="titlePlusText" 
+                    className="flex flex-col mt-[210px]"
+                >
+                    <Heading
+                        size="L"
+                        tag="h3"
+                        title="Welcome to Arch Studio"
+                        className="max-w-[600px]"
+                    />
+                    <p className="max-w-[680px] mt-10 font-medium text-grey">
+                        We have a unique network and skillset to help bring your projects 
+                        to life. Our small team of highly skilled individuals combined with 
+                        our large network put us in a strong position to deliver exceptional results.
+                        <br/>
+                        <br/>
+                        Over the past 10 years, we have worked on all kinds of projects. 
+                        From stations to high-rise buildings, we create spaces that inspire and delight.
+                        <br/>
+                        <br/>
+                        We work closely with our clients so that we understand the intricacies of each project.
+                        This allows us to work in harmony the surrounding area to create truly stunning projects that will stand the test of time.
+                    </p>
+                </div>
+                <img 
+                    src={welcomeImage} 
+                    alt="Our Building"
+                    className="w-[350px] h-[568px] ml-20 hidden lg:block" 
+                />
+            </div>
         </div>
     );
 };
--- src/components/utils/heading.jsx
+++ src/components/utils/heading.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
 
-const Heading = ({ size = 'M', color = 'black', tag = 'h1', title }) => {
+const Heading = ({ size = 'M', color = 'black', tag = 'h1', title, className }) => {
   const getSizeProps = (size) => {
     switch (size) {
       case 'XL':
         return {
-          fontSize: 'text-[250px]',
+          fontSize: 'sm:text-[16.8vw]',
           lineHeight: 'leading-[200px]',
           letterSpacing: 'tracking-[-5px]',
         };
       case 'L':
         return {
-          fontSize: 'text-[96px]',
-          lineHeight: 'leading-[80px]',
+          fontSize: 'sm:text-[6.4vw]',
+          lineHeight: 'leading-[90px]',
           letterSpacing: 'tracking-[-2px]',
         };
       case 'S':
         return {
-          fontSize: 'text-[32px]',
+          fontSize: 'sm:text-[2.1vw]',
           lineHeight: 'leading-[40px]',
           letterSpacing: 'tracking-0',
         };
       default:
         return {
-          fontSize: 'texxt-[72px]',
+          fontSize: 'sm:text-[4.8vw]',
           lineHeight: 'leading-[64px]',
           letterSpacing: 'tracking-[-2px]',
         };
@@ -37,13 +37,14 @@ const Heading = ({ size = 'M', color = 'black', tag = 'h1', title }) => {
   return (
     <Tag
         className={`
-            font-bold 
+            font-bold
+            text-4xl
             ${color}
             ${sizeProps.fontSize} 
             ${sizeProps.lineHeight}
             ${sizeProps.letterSpacing}
+            ${className}
         `}
-
     >
       {title}
     </Tag>
