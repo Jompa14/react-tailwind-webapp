1642fdf51f5621f4099e671bf7b31757d6ebc889
Jompa14 <joao.buchholz@gmail.com>
2023-04-02 22:56:10 -0300

    featured section ready

--- src/components/sections/Featured.jsx
+++ src/components/sections/Featured.jsx
@@ -1,24 +1,38 @@
 import React from 'react';
 import Card from '../utils/Card.jsx'
 import Heading from '../utils/heading.jsx'
+import Button from '../utils/Button.jsx';
+
 
 import oneImage from '../../images/portfolio/desktop/image-228b.jpg';
 import twoImage from '../../images/portfolio/desktop/image-del-sol.jpg';
 import threeImage from '../../images/portfolio/desktop/image-prototype.jpg';
 
 const Featured = () => {
-    const images = [oneImage, twoImage, threeImage]
+    const cardData = [
+        { alt: '228b Tower pic', title: '228B Tower', subtitle: 'View All Projects', img: oneImage },
+        { alt: 'Project del Sol pic', title: 'Project del Sol', subtitle: 'View All Projects2', img: twoImage },
+        { alt: 'Le Prototype pic', title: 'Le Prototype', subtitle: 'View All Projects', img: threeImage },
+    ];
+
     return(
         <div className="w-full h-full px-10 sm:px-0 my-20">
-            <Heading title="Featured"/>
+            <div className="flex justify-between align-center">
+                <Heading title="Featured"/>
+                <Button 
+                    text="See All"
+                    to="/portfolio"
+                />
+            </div>
             <div className="flex flex-col min-[1330px]:flex-row flex-wrap mt-20">
-            {images.map((image, index) => (
+            {cardData.map((card, index) => (
                 <Card 
                     key={index}
-                    img={image}
-                    title="Title"
-                    subtitle="Subtitle"
-                    className="grow h-[200px] lg:h-[450px] basis-[340px] mt-5 mr-5 last:mr-0"
+                    img={card.img}
+                    title={card.title}
+                    subtitle={card.subtitle}
+                    alt={card.alt}
+                    className="grow h-[200px] lg:h-[500px] basis-[340px] mt-5 mr-5 last:mr-0"
                 />
             ))}
             </div>
