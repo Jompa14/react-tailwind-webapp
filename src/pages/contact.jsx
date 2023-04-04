9a26db956140e90f28db3c059c7cb1ae1f7da529
Jompa14 <joao.buchholz@gmail.com>
2023-04-04 20:39:56 -0300

    image map add

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ContactSection from '../components/sections/ContactSection.jsx'
 import ContactDetails from '../components/sections/ContactDetails.jsx'
-  
+
+import imageMap from '../images/contact/desktop/image-map.png'
+
+
 const Contact = () => {
   return (
     <div>
       <ContactSection />
       <ContactDetails />
+      <img src={imageMap} alt="Our Offices" className="w-full"/>
     </div>
   );
 };
