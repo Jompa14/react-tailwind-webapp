080a9e30bae9ff01c6504b0d7bb79c10f8da2550
Jompa14 <joao.buchholz@gmail.com>
2023-04-05 08:30:38 -0300

    identation

--- src/components/utils/ContactForm.jsx
+++ src/components/utils/ContactForm.jsx
@@ -47,51 +47,51 @@ const ContactForm = () => {
 
   return (
     <form onSubmit={handleSubmit} className="w-full pr-10 sm:pr-0">
-      <div className="relative mb-3 border-b-2 border-grey-dark">
-        <input
-          type="text"
-          name="name"
-          value={name}
-          onBlur={handleBlur}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="Name"
-          className="w-full py-2 pl-2 pr-12 bg-white placeholder-grey-light"
-        />
-        {errors.name && (
-          <p className="absolute bottom-0 right-0 text-red">{errors.name}</p>
-        )}
-      </div>
-      <div className="relative mb-3 border-b-2 border-grey-dark">
-        <input
-          type="email"
-          name="email"
-          value={email}
-          onBlur={handleBlur}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
-          className="w-full py-2 pl-2 pr-12 bg-white placeholder-grey-light"
-        />
-        {errors.email && (
-          <p className="absolute bottom-0 right-0 text-red">{errors.email}</p>
-        )}
-      </div>
-      <div className="relative mb-3 border-b-2 border-grey-dark">
-        <textarea
-          name="message"
-          value={message}
-          onBlur={handleBlur}
-          onChange={(e) => setMessage(e.target.value)}
-          placeholder="Message"
-          rows={5}
-          className="w-full py-2 pl-2 pr-12 bg-white placeholder-grey-light"
-        />
-        {errors.message && (
-          <p className="absolute bottom-0 right-0 text-red">{errors.message}</p>
-        )}
-      </div>
-      <div className="flex justify-end">
-        <Button />
-      </div>
+        <div className="relative mb-3 border-b-2 border-grey-dark">
+            <input
+                type="text"
+                name="name"
+                value={name}
+                onBlur={handleBlur}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Name"
+                className="w-full py-2 pl-2 pr-12 bg-white placeholder-grey-light"
+                />
+                {errors.name && (
+                    <p className="absolute bottom-0 right-0 text-red">{errors.name}</p>
+                )}
+        </div>
+        <div className="relative mb-3 border-b-2 border-grey-dark">
+            <input
+                type="email"
+                name="email"
+                value={email}
+                onBlur={handleBlur}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email"
+                className="w-full py-2 pl-2 pr-12 bg-white placeholder-grey-light"
+            />
+            {errors.email && (
+                <p className="absolute bottom-0 right-0 text-red">{errors.email}</p>
+            )}
+        </div>
+        <div className="relative mb-3 border-b-2 border-grey-dark">
+            <textarea
+                name="message"
+                value={message}
+                onBlur={handleBlur}
+                onChange={(e) => setMessage(e.target.value)}
+                placeholder="Message"
+                rows={5}
+                className="w-full py-2 pl-2 pr-12 bg-white placeholder-grey-light"
+            />
+            {errors.message && (
+                <p className="absolute bottom-0 right-0 text-red">{errors.message}</p>
+            )}
+        </div>
+        <div className="flex justify-end">
+            <Button />
+        </div>
     </form>
   );
 };
