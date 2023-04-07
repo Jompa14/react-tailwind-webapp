70130e4349ce413a7c13dc37f8de5bb89c7fb282
Jompa14 <joao.buchholz@gmail.com>
2023-04-07 03:22:48 -0300

    white svg logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
+import logo from '../images/logo-white.svg';
+
 
 const Footer = () => {
     return(
         <footer className="relative w-full footerDoubleBg">
-            <div>
-                hello world
+            <div className="flex">
+                <div 
+                    className={`bg-grey-darkest h-[120px] w-[120px]
+                        flex justify-center items-center
+                    `}
+                >
+                    <img src={logo} alt="Arch Logo" />
+                </div>
+                
             </div>
 
         </footer>
